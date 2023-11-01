8ee664d3bbfccf83489b37b42fda63df2c3c5c41
theelementgod <theelementgod@gmail.com>
2023-11-01 11:20:17 -0700

    corrected dml code location

---
+++ movie_theatredml.sql
@@ -0,0 +1,61 @@
+INSERT INTO customer(
+    full_name,
+    age,
+    customer_address,
+    phone_num
+) VALUES (
+    'Albert Bennet',
+    12,
+    '123 main st',
+    '111-111-1111'
+), (
+    'Cindy Dawson',
+    34,
+    '456 lower ave',
+    '222-222-2222'
+);
+
+INSERT INTO movies(
+    movie_title,
+    rating,
+    runtime_mins,
+    showtime
+) VALUES (
+    'Echoes',
+    'R',
+    '135.5',
+    '12:30'
+), (
+    'Fancy Meeting You Here',
+    'PG-13',
+    '122.0',
+    '13:00'
+), (
+    'GOATS',
+    'PG',
+    '97.5',
+    '10:30'
+);
+
+INSERT INTO concessions(
+    item_purchased,
+    amount
+) VALUES (
+    'popcorn',
+    7.99
+), (
+    'hotdog',
+    9.99
+), (
+    'nachos',
+    6.99
+), (
+    'drink',
+    5.99
+), (
+    'adult beverage',
+    8.99
+), (
+    'souvenier',
+    16.99
+);
