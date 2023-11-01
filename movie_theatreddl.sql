e53648ca056ce5f54ebc563760a0c0c0852f612a
theelementgod <theelementgod@gmail.com>
2023-11-01 10:24:32 -0700

    final

--- movie_theatreddl.sql
+++ movie_theatreddl.sql
@@ -10,25 +10,22 @@ CREATE TABLE movies(
     movie_id SERIAL PRIMARY KEY,
     movie_title VARCHAR,
     rating VARCHAR,
-    runtime_mins VARCHAR
-);
-
-ALTER TABLE movies
-ADD COLUMN showtime VARCHAR;
-
-CREATE TABLE concessions(
-    transaction_id SERIAL PRIMARY KEY,
-    customer_id VARCHAR,
-    FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
-    item_purchased VARCHAR,
-    amount VARCHAR
+    runtime_mins VARCHAR,
+    showtime VARCHAR
 );
 
 CREATE TABLE tickets(
     ticket_id SERIAL PRIMARY KEY,
-    movie_title VARCHAR,
-    FOREIGN KEY (movie_title) REFERENCES movies(movie_title),
-    full_name VARCHAR,
-    FOREIGN KEY (full_name) REFERENCES customer(full_name)
+    movie_id INTEGER,
+    FOREIGN KEY (movie_id) REFERENCES movies(movie_id),
+    customer_id INTEGER,
+    FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
 );
 
+CREATE TABLE concessions(
+    transaction_id SERIAL PRIMARY KEY,
+    customer_id INTEGER,
+    FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
+    item_purchased VARCHAR,
+    amount VARCHAR
+);
